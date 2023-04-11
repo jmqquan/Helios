858dd1c287e9481d73b18837a0462cd9b44b52ba
jmqquan <731778363@qq.com>
2023-04-11 22:49:03 +0800

    complete GenerateProject.bat more

--- GenerateProject.bat
+++ GenerateProject.bat
@@ -1,2 +1,7 @@
+call rm -r .\Helios\vendor\
+call rm -r .gitmodules
+call git rm -r --cached Helios/vendor/spdlog
+call new-item .gitmodules
+call git submodule add https://github.com/gabime/spdlog Helios/vendor/spdlog
 call .\vendor\bin\premake\premake5.exe vs2022
 PAUSE
