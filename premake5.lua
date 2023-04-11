872e1106bca2c5f452856b109e37445761a2cd36
jmqquan <731778363@qq.com>
2023-04-11 21:45:26 +0800

    try with premake and submudule

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-worspace "Helios"
+workspace "Helios"
     architecture "x64"
 
     configurations
@@ -24,7 +24,7 @@ project "Helios"
         "%{prj.name}/src/**.cpp"
     }
 
-    include
+    includedirs
     {
         "%{prj.name}/vendor/spdlog/include"
     }
@@ -43,5 +43,65 @@ project "Helios"
 
         postbuildcommands
         {
-            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "SandBox")
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/SandBox")
         }
+
+    filter "configurations:Debug"
+        defines "HL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HL_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HL_DIST"
+        optimize "On"
+
+project "SandBox"
+    location "SandBox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Helios/vendor/spdlog/include",
+        "Helios/src"
+    }
+
+    links
+    {
+        "Helios"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0"
+
+        defines
+        {
+            "HL_PLATFORM_WINDOWS"
+
+        }
+
+    filter "configurations:Debug"
+        defines "HL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HL_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HL_DIST"
+        optimize "On"
